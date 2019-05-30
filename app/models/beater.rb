02211c094143793229902ca9e6132ec88fc02227
Gan Yi Zhong <ganyizhong@gmail.com>
2019-05-30 14:52:11 +0800

    add status to beater

--- app/models/beater.rb
+++ app/models/beater.rb
@@ -1,5 +1,7 @@
 class Beater < ApplicationRecord
   enum provider: %i[heroku]
+  enum status: %i[offline online]
   
+  validates :status, presence: true
   validates_format_of :url, with: URI::regexp(%w(http https))
 end
